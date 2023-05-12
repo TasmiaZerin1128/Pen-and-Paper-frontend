dd6bd9342d1faa1584598cd5282162c677b7ffef
TasmiaZerin1128 <bsse1128@iit.du.ac.bd>
2023-05-12 10:22:59 +0600

    Break down of conditional JSX inside navbar done

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -25,17 +25,48 @@ import { AuthContext } from "../../contexts/Contexts";
 import CreateBlog from "../CreateBlog/CreateBlog";
 import "./Navbar.css";
 
-export default function Navbar({ handleBlogAdd }) {
+function NavbarGeneral() {
   const navigate = useNavigate();
-  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  return (
+    <Toolbar>
+      <Typography
+        variant="h5"
+        noWrap
+        component="a"
+        href="/"
+        sx={{
+          mr: 2,
+          cursor: "pointer",
+          flexGrow: 1,
+          color: "inherit",
+          textDecoration: "none",
+        }}
+      >
+        <img
+          src="\src\assets\images\logo-sm.svg"
+          style={{ width: "4rem", marginTop: "0.5rem" }}
+        />
+      </Typography>
+      <Box sx={{ flexGrow: 0 }}>
+        <Button className="loginNav" onClick={() => navigate("/login")}>
+          Sign In
+        </Button>
+        <Button className="registerNav" onClick={() => navigate("/register")}>
+          Get Started
+        </Button>
+      </Box>
+    </Toolbar>
+  );
+}
+
+function NavbarLoggedIn({ handleBlogAdd }) {
+  const navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const { checkLoggedIn, loggedInUsername, setStatusSignedOut } =
     useContext(AuthContext);
-
-  useEffect(() => {
-    checkLoggedIn();
-  }, []);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -64,8 +95,186 @@ export default function Navbar({ handleBlogAdd }) {
   };
 
   return (
+    <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: "none", md: "flex" },
+            cursor: "pointer",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          <img
+            src="\src\assets\images\logo-sm.svg"
+            alt="logo"
+            style={{ width: "4rem", marginTop: "0.5rem" }}
+          />
+        </Typography>
+
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon style={{ color: "#863812" }} />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
+            <MenuItem style={{ backgroundColor: "transparent" }}>
+              <CreateBlog handleBlogAdd={handleBlogAdd} showToast={showToast} />
+            </MenuItem>
+          </Menu>
+        </Box>
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: "flex", md: "none" },
+            cursor: "pointer",
+            flexGrow: 1,
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          <img
+            src="\src\assets\images\logo-sm.svg"
+            alt="logo"
+            style={{ width: "4rem", marginTop: "0.5rem" }}
+          />
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <CreateBlog handleBlogAdd={handleBlogAdd} showToast={showToast} />
+        </Box>
+
+        <Box sx={{ flexGrow: 0 }}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            {checkLoggedIn ? (
+              <Typography
+                textAlign="left"
+                variant="body1"
+                sx={{
+                  padding: "0.5rem",
+                  fontSize: "15px",
+                  color: "#863812",
+                  fontFamily: "Poppins",
+                }}
+              >
+                Welcome <br />
+                <b>{loggedInUsername}</b>!
+              </Typography>
+            ) : (
+              <></>
+            )}
+            <Tooltip title="Open Profile">
+              <IconButton
+                style={{
+                  width: "10%",
+                  height: "10%",
+                  margin: "0.6rem 0 0 1rem",
+                }}
+                onClick={handleOpenUserMenu}
+                sx={{ p: 0 }}
+              >
+                <Avatar
+                  alt="profile icon"
+                  src="\src\assets\images\profile-pic2.jpg"
+                />
+              </IconButton>
+            </Tooltip>
+          </div>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            <MenuItem disabled>
+              <Typography
+                textAlign="left"
+                variant="body1"
+                sx={{ padding: 0, fontSize: "14px" }}
+              >
+                Signed in as <br />
+                <b>{loggedInUsername}</b>
+              </Typography>
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={(e) => navigate(`/profile/${loggedInUsername}`)}>
+              <ListItemIcon>
+                <AccountCircleIcon fontSize="small" />
+              </ListItemIcon>
+              <Typography textAlign="center" sx={{ padding: 0 }}>
+                Profile
+              </Typography>
+            </MenuItem>
+            <MenuItem onClick={(e) => logoutUser()}>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              <Typography textAlign="center" sx={{ padding: 0 }}>
+                Logout
+              </Typography>
+            </MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </Container>
+  );
+}
+
+export default function Navbar({ handleBlogAdd }) {
+  const { checkLoggedIn, loggedInUsername } = useContext(AuthContext);
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
+
+  return (
     <>
-      <ToastContainer transition={Zoom} limit={1} toastStyle={{ backgroundColor: "#168030" }} />
+      <ToastContainer
+        transition={Zoom}
+        limit={1}
+        toastStyle={{ backgroundColor: "#168030" }}
+      />
       <AppBar
         position="sticky"
         style={{
@@ -74,188 +283,7 @@ export default function Navbar({ handleBlogAdd }) {
           boxShadow: "none",
         }}
       >
-        { loggedInUsername ?
-        (<Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                cursor: "pointer",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              <img
-                src="\src\assets\images\logo-sm.svg"
-                alt="logo"
-                style={{ width: "4rem", marginTop: "0.5rem" }}
-              />
-            </Typography>
-
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon style={{ color: "#863812" }} />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                <MenuItem style={{ backgroundColor: "transparent" }}>
-                  <CreateBlog
-                    handleBlogAdd={handleBlogAdd}
-                    showToast={showToast}
-                  />
-                </MenuItem>
-              </Menu>
-            </Box>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                cursor: "pointer",
-                flexGrow: 1,
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              <img
-                src="\src\assets\images\logo-sm.svg"
-                alt="logo"
-                style={{ width: "4rem", marginTop: "0.5rem" }}
-              />
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              <CreateBlog handleBlogAdd={handleBlogAdd} showToast={showToast} />
-            </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <div style={{ display: "flex", flexDirection: "row" }}>
-                {checkLoggedIn ? (
-                  <Typography
-                    textAlign="left"
-                    variant="body1"
-                    sx={{
-                      padding: "0.5rem",
-                      fontSize: "15px",
-                      color: "#863812",
-                      fontFamily: "Poppins",
-                    }}
-                  >
-                    Welcome <br />
-                    <b>{loggedInUsername}</b>!
-                  </Typography>
-                ) : (
-                  <></>
-                )}
-                <Tooltip title="Open Profile">
-                  <IconButton style={{ width: '10%', height: '10%', margin: '0.6rem 0 0 1rem'}} onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar
-                      alt="profile icon"
-                      src="\src\assets\images\profile-pic2.jpg"
-                    />
-                  </IconButton>
-                </Tooltip>
-              </div>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                <MenuItem disabled>
-                  <Typography
-                    textAlign="left"
-                    variant="body1"
-                    sx={{ padding: 0, fontSize: "14px" }}
-                  >
-                    Signed in as <br />
-                    <b>{loggedInUsername}</b>
-                  </Typography>
-                </MenuItem>
-                <Divider />
-                <MenuItem
-                  onClick={(e) => navigate(`/profile/${loggedInUsername}`)}
-                >
-                  <ListItemIcon>
-                    <AccountCircleIcon fontSize="small" />
-                  </ListItemIcon>
-                  <Typography textAlign="center" sx={{ padding: 0 }}>
-                    Profile
-                  </Typography>
-                </MenuItem>
-                <MenuItem onClick={(e) => logoutUser()}>
-                  <ListItemIcon>
-                    <Logout fontSize="small" />
-                  </ListItemIcon>
-                  <Typography textAlign="center" sx={{ padding: 0 }}>
-                    Logout
-                  </Typography>
-                </MenuItem>
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>) : 
-        (<Toolbar>
-          <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                cursor: "pointer",
-                flexGrow: 1,
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              <img src='\src\assets\images\logo-sm.svg' style={{ width: '4rem', marginTop: '0.5rem' }}/>
-          </Typography>
-          <Box sx={{ flexGrow: 0 }}>
-          <Button className="loginNav" onClick={() => navigate("/login")}>Sign In</Button>
-          <Button className="registerNav" onClick={() => navigate("/register")}>Get Started</Button>
-          </Box>
-        </Toolbar>)}
+        {loggedInUsername ? ( <NavbarLoggedIn handleBlogAdd={handleBlogAdd} /> ) : ( <NavbarGeneral /> )}
       </AppBar>
     </>
   );
