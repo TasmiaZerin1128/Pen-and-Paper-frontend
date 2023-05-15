f883edd280cb2968568a82559dcecb914dc67f3a
TasmiaZerin1128 <bsse1128@iit.du.ac.bd>
2023-05-15 12:06:10 +0600

    changed base url

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ import api from "../api";
 
 const register = async (newUser) => {
     try {
-        const response = await api.post("/auth/register", newUser, { withCredentials: true });
+        const response = await api.post("/api/v1/auth/register", newUser, { withCredentials: true });
         return response;
     } catch (err) {
         return err.response;
@@ -11,7 +11,7 @@ const register = async (newUser) => {
 
 const login = async (loginUser) => {
     try {
-        const response = await api.post("/auth/login", loginUser, { withCredentials: true });
+        const response = await api.post("/api/v1/auth/login", loginUser, { withCredentials: true });
         return response;
     } catch (err) {
         return err.response;
@@ -20,7 +20,7 @@ const login = async (loginUser) => {
 
 const logout = async () => {
     try {
-        const response = await api.post("auth/logout");
+        const response = await api.post("/api/v1/auth/logout");
         return response;
     } catch (err) {
         return err.response;
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       // '/api': 'http://192.168.1.92:3000',
-      // '/api': "https://penpaper.cyclic.app/"
-      '/api': {
-        target: 'https://penpaper.cyclic.app',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
-      },
+      '/api': "https://penpaper.cyclic.app/"
     },
   },
   plugins: [react()],
