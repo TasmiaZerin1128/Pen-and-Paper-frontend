393c27f2068dcf63dc0168d4fe0e71ec544c040b
TasmiaZerin1128 <bsse1128@iit.du.ac.bd>
2023-05-15 17:12:31 +0600

    test proxy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,18 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    proxy: {
-    //   '/api': 'http://192.168.1.92:3000',
-      '/api': "https://penpaper.cyclic.app"
-    },
+    // proxy: {
+    // //   '/api': 'http://192.168.1.92:3000',
+    //   '/api': "https://penpaper.cyclic.app"
+ 
+    // },
+    proxy : {
+      '/api': {
+        target: 'https://penpaper.cyclic.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
   },
   plugins: [react()],
 })
