de0bdde9761a42ac0ecbb0ffeb26d249ba412283
TasmiaZerin1128 <bsse1128@iit.du.ac.bd>
2023-05-16 14:13:35 +0600

    check vite config

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,14 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    // proxy: {
-    // //   '/api': 'http://192.168.1.92:3000',
-    //   '/api': "https://penpaper.cyclic.app"
- 
-    // },
-    proxy : {
-      '/api': {
-        target: 'https://penpaper.cyclic.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    }
-  },
+  // server: {
+  //   proxy : {
+  //     '/api': {
+  //       target: 'https://penpaper.cyclic.app',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   }
+  // },
   plugins: [react()],
 })
